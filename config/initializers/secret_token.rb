e3d7259e70ef1ecefb63864e371cada6b55da2b7
Patti Jurgensen & Angela Yen <student@epicodus.com>
2014-03-31 08:53:41 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikipages::Application.config.secret_key_base = '7dc632eb98a3acc358935fbea7f4fd2f992d622fe5807ccbdc923063316d09cb5f4fc7861dfde4da4e5a5ec4b1de4e36ea055728c936851dd12c7e0b8dbad9a8'
